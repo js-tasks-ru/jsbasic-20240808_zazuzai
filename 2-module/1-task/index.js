a2901b24de7dda8dfe05f0f264588c91573bb01e
zazuzai <pomudra@gmail.com>
2024-08-19 17:44:38 +0300

    Функцию для суммирования всех зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let key in salaries) {
+    let salary = salaries[key];
+
+    if (typeof salary === 'number' && isFinite(salary)) {
+      sum += salary;
+    }
+  }
+
+  return sum;
 }
