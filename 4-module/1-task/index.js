ecfce645aa03d702b837a882b898c6328182d7b5
zazuzai <pomudra@gmail.com>
2024-08-28 16:05:17 +0300

    Занятие 4: Знакомство с DOM

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  for (const friend of friends) {
+    const li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  }
+
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+
+  rows.forEach(row => {
+    const statusCell = row.querySelector('td[data-available]');
+    const genderCell = row.querySelector('td:nth-child(3)');
+    const ageCell = row.querySelector('td:nth-child(2)');
+
+    if (statusCell) {
+      if (statusCell.dataset.available === 'true') {
+        row.classList.add('available');
+      } else if (statusCell.dataset.available === 'false') {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.setAttribute('hidden', '');
+    }
+
+    if (genderCell) {
+      if (genderCell.textContent === 'm') {
+        row.classList.add('male');
+      } else if (genderCell.textContent === 'f') {
+        row.classList.add('female');
+      }
+    }
+
+    if (ageCell) {
+      if (parseInt(ageCell.textContent) < 18) {
+        row.style.textDecoration = 'line-through';
+      }
+    }
+  });
+
+
 }
