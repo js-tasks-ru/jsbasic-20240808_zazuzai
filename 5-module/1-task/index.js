d2381b55d533e3c070874a09b0b99ef33ccd47c8
zazuzai <pomudra@gmail.com>
2024-09-03 17:35:44 +0300

    Спрятать кнопку с использованием атрибута hidden

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
-  // ваш код...
+  let btnHidden = document.querySelectorAll('.hide-self-button');
+
+  btnHidden.forEach(button => {
+    button.addEventListener('click', () => {
+      button.hidden = true;
+    });
+  });
+
 }
